68795268f2731b9c7ec638b37c169ba5c8eaf419
pierre1326 <pierre1326@hotmail.com>
2020-07-28 21:21:28 -0600

    Cita

--- src/main/java/modelo/Cita.java
+++ src/main/java/modelo/Cita.java
@@ -28,10 +28,6 @@ public class Cita {
         return observacion;
     }
 
-  public int getIdCita() {
-    return idCita;
-  }
-
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
     }
@@ -48,45 +44,13 @@ public class Cita {
         this.estado = estado;
     }
 
+    public String getEstado() {
+        return estado;
+    }
+    
     @Override
     public String toString() {
         return "citas{" + "especialidad=" + especialidad + ", fechaHora=" + fechahora + ", observacion=" + observacion + ", estado=" + estado + '}';
     }
 
-  public String getEspecialidad() {
-      return especialidad;
-  }
-
-  public LocalDateTime getFechaHora() {
-      return fechaHora;
-  }
-
-  public String getObservacion() {
-      return observacion;
-  }
-
-  public String getEstado() {
-      return estado;
-  }
-
-  public void setEspecialidad(String especialidad) {
-      this.especialidad = especialidad;
-  }
-
-  public void setFechaHora(LocalDateTime fechaHora) {
-      this.fechaHora = fechaHora;
-  }
-
-  public void setObservacion(String observacion) {
-      this.observacion = observacion;
-  }
-
-  public void setEstado(String estado) {
-      this.estado = estado;
-  }
-
-  @Override public String toString() {
-      return "citas{" + "especialidad=" + especialidad + ", fechaHora=" + fechaHora +
-        ", observacion=" + observacion + ", estado=" + estado + '}';
-  }
 }
