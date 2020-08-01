28369b09564af229e4ac1088a23b8073af76af6d
HamsterObeso <43190712+HamsterObeso@users.noreply.github.com>
2020-08-01 11:22:39 -0600

    Especialidades mal hechas como siempre

---
+++ src/main/java/modelo/TablaEspecialidad.java
@@ -0,0 +1,32 @@
+package modelo;
+
+/**
+ *
+ * @author Muro
+ */
+public class TablaEspecialidad {
+  private int id = -1;
+  private String nombre;
+
+  public TablaEspecialidad(int id, String nombre) {
+    this.id = id;
+    this.nombre = nombre;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+  
+}
