b658182d084752bb8b2e4b1d76576e096c4970f2
HamsterObeso <43190712+HamsterObeso@users.noreply.github.com>
2020-08-01 21:51:00 -0600

    intentos de no suicidarme

--- src/main/java/controlador/GestionEspecialidad.java
+++ src/main/java/controlador/GestionEspecialidad.java
@@ -56,7 +56,7 @@ public class GestionEspecialidad {
       model.put("mensaje", "Se agregó la nueva área de trabajo.");
     }
     loadTable(model);
-    return "redirect:/especialidades";
+    return "redirect:/gestionEspecialidad";
   }
   
   private String actualizar(Map<String, Object> model, FormGestionEspecialidad form) {
@@ -70,7 +70,7 @@ public class GestionEspecialidad {
       model.put("mensaje", "Se actualizo el área");
     }
     loadTable(model);
-    return "redirect:/especialidades";
+    return "redirect:/gestionEspecialidad";
   }
   
   private String eliminar(Map<String, Object> model, FormGestionEspecialidad form) {
@@ -83,7 +83,7 @@ public class GestionEspecialidad {
       model.put("mensaje", "Se elimino el área");
     }
     loadTable(model);
-    return "redirect:/especialidades";
+    return "redirect:/gestionEspecialidad";
   }
   
   private void loadTable(Map<String, Object> model) {
